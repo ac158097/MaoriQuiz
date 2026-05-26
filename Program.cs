aac481cdb83aa30354ff9100324c49201b6cc190
ac158097 <ac158097@avcol.school.nz>
2026-05-26 13:49:44 +1200

    Added name input

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
         }
     }
 }
