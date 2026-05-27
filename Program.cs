2227a3fd8cf052de304c69e1741d0ddc3e6187af
ac158097 <ac158097@avcol.school.nz>
2026-05-27 12:11:53 +1200

    added basic regex name validation and a (currently broken) ansi formatting simplifier

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MaoriQuiz
+﻿using System.Text.RegularExpressions;
+
+namespace MaoriQuiz
 {
     internal class Program
     {
@@ -7,13 +9,37 @@
             string name;
             do
             {
-                Console.Write("Enter Name: ");
-                name = Console.ReadLine();
+                Console.Write($"Enter Your {Fancify("Full", isBold: true)} Name: ");
+                name = Console.ReadLine().Trim();
             } while (!ValidName(name));
         }
 
-        static bool ValidName(string nameToTest) {
-            return true;
+        static bool ValidName(string nameToTest)
+        {
+            Regex nameRegex = new Regex(@"^[a-z] [a-z]+$", RegexOptions.IgnoreCase);
+            if (nameRegex.Match(nameToTest).Length == nameToTest.Length && nameToTest.Length > 3 && nameToTest.Length < 20) {
+                return true;
+            }
+            return false; 
+        }
+
+        /*
+        color numbers:
+        0: white
+        30: black
+        31: red
+        32: green
+        33: yellow
+        34: blue
+        35: purple
+        36: cyan
+        37: white
+        method for ansi formatting but somewhat simpler
+        */
+        static string Fancify(string stringToApplyTo, bool isBold = false, bool isUnderline = false, int colorNum = 37) {
+            string stringANSIFormatted = $"\e[{Convert.ToInt16(isBold)};{colorNum}{stringToApplyTo}\e[0m";
+            //string stringANSIFormatted = $"{stringToApplyTo}";
+            return stringANSIFormatted;
         }
     }
 }
