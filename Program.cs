a9377c457a76486f1c5bb5a6ea2ac40e1ba82ccc
ac158097 <ac158097@avcol.school.nz>
2026-05-27 12:21:23 +1200

    fixed name validation, somehwhat finished fancifier

--- Program.cs
+++ Program.cs
@@ -9,15 +9,15 @@ namespace MaoriQuiz
             string name;
             do
             {
-                Console.Write($"Enter Your {Fancify("Full", isBold: true)} Name: ");
+                Console.Write($"Enter Your {Fancify("Full", isBold: true, isUnderline: true)} Name: ");
                 name = Console.ReadLine().Trim();
             } while (!ValidName(name));
         }
 
         static bool ValidName(string nameToTest)
         {
-            Regex nameRegex = new Regex(@"^[a-z] [a-z]+$", RegexOptions.IgnoreCase);
-            if (nameRegex.Match(nameToTest).Length == nameToTest.Length && nameToTest.Length > 3 && nameToTest.Length < 20) {
+            Regex nameRegex = new Regex(@"[a-z]* [a-z]*", RegexOptions.IgnoreCase);
+            if ((nameRegex.Match(nameToTest).Length == nameToTest.Length) && (nameToTest.Length > 3 && nameToTest.Length < 20)) {
                 return true;
             }
             return false; 
@@ -37,9 +37,11 @@ namespace MaoriQuiz
         method for ansi formatting but somewhat simpler
         */
         static string Fancify(string stringToApplyTo, bool isBold = false, bool isUnderline = false, int colorNum = 37) {
-            string stringANSIFormatted = $"\e[{Convert.ToInt16(isBold)};{colorNum}{stringToApplyTo}\e[0m";
-            //string stringANSIFormatted = $"{stringToApplyTo}";
-            return stringANSIFormatted;
+            if (isUnderline == true) {
+                return $"\e[4;{colorNum}m\e[{Convert.ToInt16(isBold)};{colorNum}m{stringToApplyTo}\e[0m";
+            } else {
+                return $"\e[{Convert.ToInt16(isBold)};{colorNum}m{stringToApplyTo}\e[0m";
+            }
         }
     }
 }
